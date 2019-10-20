92dbd4e5d9c50120358333d11b5d21722e64d165
gakiloroth <gakiloroth@yahoo.com>
2019-10-20 01:38:30 -0700

    moved urls to config.json, readme update

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 ## WIP
 Learning how to make a discord bot :) This is a Legends of Runeterra Discord bot.
+There is currently a typo with how Riot named their region image, its spelled "zuan" when its supposed to be "zaun".
 
 ### Current Functionality:
 - Getting card info
 - Getting keyword info
+- Getting full card art
 - Basic API info (local player current match, last match, deck)
 
 ### This bot requires a config.json with these values:
 - token: bot's auth token
 - prefix: the prefix for bot commands (ie "!" if the commands are "!cardname zed")
 - ownerID: your discord id
+- corepackageurl: "https://dd.b.pvp.net/datadragon-core-en_us.zip"
+- setbundlefullurl: "https://dd.b.pvp.net/datadragon-set1-en_us.zip"
 - globals: "./data/core-en-us/en_us/data/globals-en_us.json"
 - regionIcons: "./data/core-en-us/en_us/img/regions/"
 - set: "./data/set-full/en_us/data/set1-en_us.json"
@@ -22,6 +26,6 @@ Example:
 <img src="https://i.imgur.com/XJdxIuc.png" width="400">
 
 ### Disclaimer
-This bot isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games
+LoR-Bot isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games
 or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are
 trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
--- bot.js
+++ bot.js
@@ -15,8 +15,9 @@ if(process.argv[2] === 'true'){
   redownloadJSON = true;
 }
 
-var core_package_url = 'https://dd.b.pvp.net/datadragon-core-en_us.zip';
-var set_bundle_full_url = 'https://dd.b.pvp.net/datadragon-set1-en_us.zip';
+// data packages retrieval
+var core_package_url = config.corepackageurl;
+var set_bundle_full_url = config.setbundlefullurl;
 
 var AdmZip = require('adm-zip');
 var request = require('request');
@@ -549,33 +550,3 @@ process.on('unhandledRejection', error => {
 });
 
 client.login(config.token);
-
-/*const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-
-   //Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-
-  .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
-
-   //Takes a Date object, defaults to current date.
-
-  .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 1024 characters.")
-
-   //Inline fields may not display as inline if the thumbnail and/or image is too big.
-
-  .addField("Inline Field", "They can also be inline.", true)
-
-   //Blank field, useful to create some space.
-
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-
-  message.channel.send({embed});*/
